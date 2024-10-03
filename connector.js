c8387f162be7639aee887f7a55ecf5a3c225a867
Ukesh Mainali <87257917+UkeshMainali@users.noreply.github.com>
2024-10-04 00:25:55 +0545

    Update connector.js

--- connector.js
+++ connector.js
@@ -381,7 +381,7 @@ function updateCard() {
     // Determine the state of the flashcard based on isFlippedMode
     if (isFlippedMode) {
         flashcard.classList.add('flipped'); // Show back side
-        flipButton.innerText = "Guess the meaning"; // Change button text to "Flipped"
+        flipButton.innerText = "Guess the word"; // Change button text to "Flipped"
     } else {
         flashcard.classList.remove('flipped'); // Show front side
         flipButton.innerText = "Guess from meaning"; // Reset button text
